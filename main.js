b39e13d4afb4fc5a261cc8ae7e52e3535795916a
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2022-06-10 14:12:11 +0000

    final touches

--- main.js
+++
@@ -1,7 +0,0 @@
-const openBtn = document.querySelector('#open')
-const nav = document.querySelector('#nav');
-openBtn.addEventListener('click', openNav);
-
-function openNav() {
-    nav.lastChild.innerHTML = none;
-}
