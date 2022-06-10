a6af402186a055df4973cab27d8025f9156274dc
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2022-06-10 06:26:14 +0000

    Side Navbar mobile PT1

---
+++ main.js
@@ -0,0 +1,7 @@
+const openBtn = document.querySelector('#open')
+const nav = document.querySelector('#nav');
+openBtn.addEventListener('click', openNav);
+
+function openNav() {
+    nav.lastChild.innerHTML = none;
+}
